7dc9fbb44cdab0848be695a455f20b80d17f98a0
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-04-12 10:52:06 +0200

    fix(autofold): only issue folding to LSPs supporting it

--- lua/origami/features/autofold-comments-imports.lua
+++ lua/origami/features/autofold-comments-imports.lua
@@ -12,14 +12,13 @@ vim.api.nvim_create_autocmd("LspNotify", {
 	group = vim.api.nvim_create_augroup("origami-autofolds", { clear = true }),
 	callback = function(ctx)
 		if ctx.data.method ~= "textDocument/didOpen" then return end
+		local client = assert(vim.lsp.get_client_by_id(ctx.data.client_id))
+		if not client:supports_method("textDocument/foldingRange") then return end
 
 		local kinds = require("origami.config").config.autoFold.kinds
-
-		vim.defer_fn(function()
-			for _, kind in ipairs(kinds) do
-				pcall(vim.lsp.foldclose, kind, vim.fn.bufwinid(ctx.buf))
-			end
-			vim.cmd.normal { "zv", bang = true } -- unfold under cursor
-		end, 1)
+		for _, kind in ipairs(kinds) do
+			pcall(vim.lsp.foldclose, kind, vim.fn.bufwinid(ctx.buf))
+		end
+		vim.cmd.normal { "zv", bang = true } -- unfold under cursor
 	end,
 })
