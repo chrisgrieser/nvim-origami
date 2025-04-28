1d17e28e31d248d1ea5c6e6bb83be8dad0250453
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-04-28 22:03:15 +0200

    fix(autofold): check for valid window and buffer

--- lua/origami/features/autofold-comments-imports.lua
+++ lua/origami/features/autofold-comments-imports.lua
@@ -12,6 +12,8 @@ vim.api.nvim_create_autocmd("LspNotify", {
 	group = vim.api.nvim_create_augroup("origami-autofolds", { clear = true }),
 	callback = function(ctx)
 		if ctx.data.method ~= "textDocument/didOpen" then return end
+		if vim.bo[ctx.buf].buftype ~= "" or not vim.api.nvim_buf_is_valid(ctx.buf) then return end
+
 		-- not using `lsp.get_clients_by_id` to additionally check for the correct
 		-- buffer (can change in quick events)
 		local client = vim.lsp.get_clients({ bufnr = ctx.buf, id = ctx.data.client_id })[1]
@@ -19,11 +21,13 @@ vim.api.nvim_create_autocmd("LspNotify", {
 		if not client:supports_method("textDocument/foldingRange") then return end
 
 		local kinds = require("origami.config").config.autoFold.kinds
+		local winid = vim.fn.bufwinid(ctx.buf)
+		if not winid or vim.api.nvim_win_is_valid(winid) then return end
 		for _, kind in ipairs(kinds) do
-			pcall(vim.lsp.foldclose, kind, vim.fn.bufwinid(ctx.buf))
+			pcall(vim.lsp.foldclose, kind, winid)
 		end
 
 		-- unfold under cursor
-		vim.defer_fn(function() vim.cmd.normal { "zv", bang = true } end, 1)
+		vim.schedule(function() vim.cmd.normal { "zv", bang = true } end)
 	end,
 })
