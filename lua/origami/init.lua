9509c3dc5d37ae595338a97c13aacdd675325597
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-06-19 16:48:29 +0200

    chore: remove outdated reference file

--- lua/origami/init.lua
+++ lua/origami/init.lua
@@ -16,12 +16,14 @@ local M = {}
 ---@param userConfig? Origami.config
 function M.setup(userConfig) require("origami.config").setup(userConfig) end
 
--- make keymaps accessible from `require("origami")` for easier remapping
+--------------------------------------------------------------------------------
+
+-- make these functions accessible from `require("origami")` for easier remapping
 function M.l() require("origami.features.fold-keymaps").l() end
 function M.h() require("origami.features.fold-keymaps").h() end
-
----@param type "special"|"all"?
-function M.inspectLspFolds(type) require("origami.features.inspect-folds").inspectLspFolds(type) end
+function M.inspectLspFolds(type) ---@param type "special"|"all"?
+	require("origami.inspect-folds").inspectLspFolds(type)
+end
 
 --------------------------------------------------------------------------------
 return M
