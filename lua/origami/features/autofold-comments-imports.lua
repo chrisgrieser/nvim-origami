f32ae981e691be754852e846018cad8e648d9470
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-08-06 18:50:45 +0200

    fix(autofold): schedule `vim.lsp.foldclose`

--- lua/origami/features/autofold-comments-imports.lua
+++ lua/origami/features/autofold-comments-imports.lua
@@ -6,21 +6,23 @@ vim.api.nvim_create_autocmd("LspNotify", {
 		if ctx.data.method ~= "textDocument/didOpen" then return end
 		if vim.bo[ctx.buf].buftype ~= "" or not vim.api.nvim_buf_is_valid(ctx.buf) then return end
 
-		-- not using `lsp.get_clients_by_id` to additionally check for the correct
-		-- buffer (can change in quick events)
-		local client = vim.lsp.get_clients({ bufnr = ctx.buf, id = ctx.data.client_id })[1]
-		if not client then return end
-		if not client:supports_method("textDocument/foldingRange") then return end
+		vim.schedule(function()
+			-- not using `lsp.get_clients_by_id` to additionally check for the correct
+			-- buffer (can change in quick events)
+			local client = vim.lsp.get_clients({ bufnr = ctx.buf, id = ctx.data.client_id })[1]
+			if not client then return end
+			if not client:supports_method("textDocument/foldingRange") then return end
 
-		local kinds = require("origami.config").config.autoFold.kinds
-		local winid = vim.fn.bufwinid(ctx.buf)
-		if not winid or not vim.api.nvim_win_is_valid(winid) then return end
-		for _, kind in ipairs(kinds) do
-			local success = pcall(vim.lsp.foldclose, kind, winid)
-			vim.b[ctx.buf].origami_has_autofolded = success
-		end
-
-		-- unfold under cursor
-		vim.schedule(function() vim.cmd.normal { "zv", bang = true } end)
+			local kinds = require("origami.config").config.autoFold.kinds
+			local winid = vim.fn.bufwinid(ctx.buf)
+			if not winid or not vim.api.nvim_win_is_valid(winid) then return end
+			local success = false
+			for _, kind in ipairs(kinds) do
+				success = pcall(vim.lsp.foldclose, kind, winid)
+				vim.b[ctx.buf].origami_has_autofolded = success
+			end
+			-- unfold under cursor
+			if success then vim.cmd.normal { "zv", bang = true } end
+		end)
 	end,
 })
