eba1fb2662fac78dc65f6182f68e82830a3d4ecf
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-09-14 17:23:38 +0200

    fix(autofold): do not unfold under cursor since unreliable (#37)

--- lua/origami/features/autofold-comments-imports.lua
+++ lua/origami/features/autofold-comments-imports.lua
@@ -2,7 +2,6 @@ vim.api.nvim_create_autocmd("LspNotify", {
 	desc = "Origami: Close imports and comments on load",
 	group = vim.api.nvim_create_augroup("origami.autofolds", { clear = true }),
 	callback = function(ctx)
-		if vim.b[ctx.buf].origami_has_autofolded then return end
 		if ctx.data.method ~= "textDocument/didOpen" then return end
 		if vim.bo[ctx.buf].buftype ~= "" or not vim.api.nvim_buf_is_valid(ctx.buf) then return end
 
@@ -10,19 +9,17 @@ vim.api.nvim_create_autocmd("LspNotify", {
 			-- not using `lsp.get_clients_by_id` to additionally check for the correct
 			-- buffer (can change in quick events)
 			local client = vim.lsp.get_clients({ bufnr = ctx.buf, id = ctx.data.client_id })[1]
-			if not client then return end
-			if not client:supports_method("textDocument/foldingRange") then return end
+			if not client or not client:supports_method("textDocument/foldingRange") then return end
 
 			local kinds = require("origami.config").config.autoFold.kinds
 			local winid = vim.fn.bufwinid(ctx.buf)
 			if not winid or not vim.api.nvim_win_is_valid(winid) then return end
-			local success = false
 			for _, kind in ipairs(kinds) do
-				success = pcall(vim.lsp.foldclose, kind, winid)
+				local success = pcall(vim.lsp.foldclose, kind, winid)
 				vim.b[ctx.buf].origami_has_autofolded = success
 			end
-			-- unfold under cursor
-			if success then vim.cmd.normal { "zv", bang = true } end
+			-- cannot reliably unfold under cursor due `vim.lsp.foldclose` being
+			-- async without callback, see #37
 		end)
 	end,
 })
